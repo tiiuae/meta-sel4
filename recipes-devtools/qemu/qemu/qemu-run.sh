1763c1d0f12b50e99ed47e13c48bbbc449b94747
Hannu Lyytinen <hannu.lyytinen@unikie.com>
2021-12-01 13:28:30 +0200

    HYPR-158 Add seL4/virtio support
    Signed-off-by: Hannu Lyytinen <hannux@ssrc.tii.ae>

---
+++ recipes-devtools/qemu/qemu/qemu-run.sh
@@ -0,0 +1,46 @@
+#! /bin/sh
+
+# guest memory size in megabytes
+# TODO: autoprobe from uio
+MEMSZ=128
+
+# rootfs for guest
+ROOTFS='/myimg.qcow2'
+# directory shared to guest
+SHARED='/shared'
+
+# CAmkES dataports
+modprobe uio
+modprobe connection
+
+ulimit -c unlimited
+
+exec /usr/bin/qemu-system-aarch64 \
+	`# minicom already uses ^A, change monitor escape to ^T` \
+	-echr 0x14 \
+	`# do not touch these` \
+	--accel sel4 \
+	-M virt \
+	`# use CAmkES dataport for the system memory` \
+	-object memory-backend-file,id=virt.ram,size=${MEMSZ}M,mem-path=/dev/uio1,share=on \
+	-machine memory-backend=virt.ram \
+	-m ${MEMSZ}m \
+	`# virtio-blk device for guest rootfs` \
+	-device virtio-blk-pci,drive=drive0,id=virtblk0 \
+	-drive format=qcow2,file=${ROOTFS},if=none,id=drive0 \
+	`# share directory to guest over 9P` \
+	-virtfs local,path=${SHARED},mount_tag=shared,security_model=mapped-xattr \
+	`# debug messages, use monitor to read them` \
+	-chardev ringbuf,id=debug,size=64K \
+	`# enable multiplexer on stdio to have both guest and monitor` \
+	-chardev stdio,id=mon,mux=on,signal=off \
+	`# enable monitor` \
+	-mon chardev=mon,mode=readline \
+	`# virtio-console` \
+	-device virtio-serial-pci \
+	-device virtconsole,chardev=mon,id=console0,name=qemu.uservm \
+	`# no need for these` \
+	-vga none \
+	-display none \
+	-serial none \
+	`# that's it!`
